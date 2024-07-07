9e19ffab3131f6f6e385daf94dd381b549a15622
barrett <onkoe@protonmail.com>
2024-07-07 17:12:08 -0500

    feat(macros/util): fn to create Paths
    these are annoying to set up manually for whatever reason. oh well

--- macros/src/util.rs
+++ macros/src/util.rs
@@ -2,13 +2,30 @@
 //!
 //! Some utilities to help out with writing these macros.
 
-use proc_macro::{Span, TokenStream};
+use proc_macro::TokenStream;
 use proc_macro2::Span as Span2;
 use quote::quote_spanned;
+use syn::{punctuated::Punctuated, Ident, Path, PathSegment};
 
 pub fn comp_time_err(span: Span2, msg: &str) -> TokenStream {
     quote_spanned! {span=>
         compile_error!(#msg);
     }
     .into()
+}
+
+pub fn create_path(span: Span2, ident_strs: &[&str]) -> Path {
+    Path {
+        leading_colon: None,
+        segments: {
+            let mut p = Punctuated::new();
+            for s in ident_strs {
+                p.push(PathSegment {
+                    ident: Ident::new(s, span),
+                    arguments: syn::PathArguments::None,
+                });
+            }
+            p
+        },
+    }
 }
