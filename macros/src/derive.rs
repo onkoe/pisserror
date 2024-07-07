f2d96b266b56ffa27629523b763210a709e5bab1
barrett <onkoe@protonmail.com>
2024-07-07 18:44:00 -0500

    refactor(macros/wip): improve error handling
    this blog is incredible: https://blog.turbo.fish/proc-macro-error-handling/

--- macros/src/derive.rs
+++ macros/src/derive.rs
@@ -2,64 +2,63 @@
 //!
 //! Contains the `#[derive(Error)]` part of pisserror.
 
-use proc_macro::TokenStream;
+use proc_macro2::TokenStream as TokenStream2;
 use quote::quote_spanned;
-use syn::{parse_macro_input, spanned::Spanned as _, DeriveInput, Item};
+use syn::{spanned::Spanned as _, DeriveInput, Item};
 
 use crate::{
     error::{cause, description, source},
     util::create_path,
 };
 
-pub fn derive_error(input: TokenStream) -> TokenStream {
-    let input = parse_macro_input!(input as DeriveInput);
+pub fn derive_error(input: DeriveInput) -> syn::Result<TokenStream2> {
     let input_span = input.span();
+    let input_ident = input.ident.clone();
 
     // make sure we've been given an enum.
-    if let Item::Enum(item) = Item::from(input) {
-        let after_span = item.brace_token.span.close();
-
-        let name = item.ident;
-        let variants = item.variants;
-
-        // Rust's Error type is defined as: `Error: Debug + Display`. to satisfy
-        // `Debug`, we need to make sure the enum implements it.
-        //
-        // let debug_check = ...; // TODO: do some fancy checks. maybe assertions with an internal type?
-        //
-        // HEY! the compiler already does this for us! a nice error message might be preferable, though!
-
-        // assemble the match arms
-        let source = source(after_span, variants.iter()); // TODO: check after_span
-        let description = description();
-        let cause = cause();
-
-        let error_path = create_path(input_span, &["std", "error", "Error"]);
-        let display_path = create_path(input_span, &["core", "fmt", "Display"]);
-
-        // put all those together!
-        let impl_block = quote_spanned! {after_span=>
-            use #error_path;
-
-            #[automatically_derived]
-            impl #error_path for #name {
-                #source
-                #description
-                #cause
-            }
-
-            #[automatically_derived]
-            impl #display_path for #name {
-                compile_error!("TODO: Display is not yet implemented.")
-            }
-        };
-
-        impl_block.into()
-    } else {
+    let Item::Enum(item) = Item::from(input) else {
         // remind user to use an enum.
-        quote_spanned! {input_span=>
-            compile_error!("You must use an `enum` when deriving types with `pisserror`.");
+        return Err(syn::Error::new_spanned(
+            input_ident,
+            "You must use an `enum` when deriving types with `pisserror`.",
+        ));
+    };
+
+    let after_span = item.brace_token.span.close();
+
+    let name = item.ident;
+    let variants = item.variants;
+
+    // Rust's Error type is defined as: `Error: Debug + Display`. to satisfy
+    // `Debug`, we need to make sure the enum implements it.
+    //
+    // let debug_check = ...; // TODO: do some fancy checks. maybe assertions with an internal type?
+    //
+    // HEY! the compiler already does this for us! a nice error message might be preferable, though!
+
+    // assemble the match arms
+    let source = source(after_span, variants.iter()); // TODO: check after_span
+    let description = description();
+    let cause = cause();
+
+    let error_path = create_path(input_span, &["std", "error", "Error"]);
+    let display_path = create_path(input_span, &["core", "fmt", "Display"]);
+
+    // put all those together!
+    let impl_block = quote_spanned! {after_span=>
+        use #error_path;
+
+        #[automatically_derived]
+        impl #error_path for #name {
+            #source
+            #description
+            #cause
         }
-        .into()
-    }
+
+        #[automatically_derived]
+        impl #display_path for #name {
+        }
+    };
+
+    Ok(impl_block)
 }
--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -5,6 +5,7 @@
 //! In a fair world, this would be done with [reflection](https://t.co/vTvZ8FVEak).
 
 use proc_macro::TokenStream;
+use syn::{parse_macro_input, DeriveInput};
 
 mod derive;
 pub(crate) mod display;
@@ -15,5 +16,11 @@ mod util;
 #[proc_macro_derive(Error, attributes(error, from))]
 pub fn derive_error(input: TokenStream) -> TokenStream {
     // FIXME: all derives must be in the root module for some reason...
-    derive::derive_error(input)
+    let input = parse_macro_input!(input as DeriveInput);
+
+    match derive::derive_error(input) {
+        Ok(k) => k,
+        Err(e) => e.into_compile_error(),
+    }
+    .into()
 }
