a95f6428db3c29ad9a7253b35d1cf505a7b898ef
barrett <onkoe@protonmail.com>
2024-07-07 17:11:27 -0500

    feat(macros): switch on "extra-traits" syn feature
    this lets us compare paths, which i'm about to use. :)

--- macros/src/util.rs
+++ macros/src/util.rs
@@ -3,11 +3,12 @@
 //! Some utilities to help out with writing these macros.
 
 use proc_macro::{Span, TokenStream};
+use proc_macro2::Span as Span2;
 use quote::quote_spanned;
 
-pub fn comp_time_err(span: Span, msg: &str) -> TokenStream {
-    quote_spanned! {span.into()=>
-        compile_error!(msg);
+pub fn comp_time_err(span: Span2, msg: &str) -> TokenStream {
+    quote_spanned! {span=>
+        compile_error!(#msg);
     }
     .into()
 }
