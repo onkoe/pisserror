eb82fbbc2b7376fa453ca338650c9198afd51dc1
Barrett <onkoe@protonmail.com>
2024-07-08 13:13:28 -0500

    test(from): add struct enum version of test

--- macros/tests/from.rs
+++ macros/tests/from.rs
@@ -3,7 +3,16 @@ use std::error::Error;
 
 // This one only has one attr, so it works fine.
 #[derive(Debug, Error)]
-enum SomeError {
-    #[error("hello")]
+enum SomeTupleEnumError {
+    #[error("hello tuple enum")]
     OneAttrOneField(#[from] std::io::Error),
+}
+
+#[derive(Debug, Error)]
+enum SomeStructEnumError {
+    #[error("hello struct enum")]
+    FromIo {
+        #[from]
+        from: std::io::Error,
+    },
 }
