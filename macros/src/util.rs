1cab6481d8924a17d672ce53f92463c0c3c49dd1
Barrett <onkoe@protonmail.com>
2024-07-07 23:02:10 -0500

    chore(macros): remove unused helper fn
    the syn::Error stuff is better ;)

--- macros/src/util.rs
+++ macros/src/util.rs
@@ -2,17 +2,8 @@
 //!
 //! Some utilities to help out with writing these macros.
 
-use proc_macro::TokenStream;
 use proc_macro2::Span as Span2;
-use quote::quote_spanned;
 use syn::{punctuated::Punctuated, Ident, Path, PathSegment};
-
-pub fn comp_time_err(span: Span2, msg: &str) -> TokenStream {
-    quote_spanned! {span=>
-        compile_error!(#msg);
-    }
-    .into()
-}
 
 pub fn create_path(span: Span2, ident_strs: &[&str]) -> Path {
     Path {
