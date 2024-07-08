b60a8c61c59d51a1239109aea546a155a91ad959
Barrett <onkoe@protonmail.com>
2024-07-08 17:32:30 -0500

    test(macros/from): add some tests

--- macros/src/from.rs
+++ macros/src/from.rs
@@ -15,7 +15,7 @@ pub(crate) fn fields_with_from_attrs(
     span: Span2,
     variants: &Punctuated<Variant, Comma>,
 ) -> syn::Result<Vec<(&Variant, Option<&Type>)>> {
-    let from_attr = create_path(span, &["from"]);
+    let from_attr: syn::Path = create_path(span, &["from"]);
     let mut list = Vec::with_capacity(variants.len());
 
     for variant in variants {
@@ -48,4 +48,69 @@ pub(crate) fn fields_with_from_attrs(
 
     // return the list we made!
     Ok(list)
+}
+#[cfg(test)]
+mod tests {
+    mod field_checking_tests {
+        use crate::from::fields_with_from_attrs;
+        use syn::{parse_quote, spanned::Spanned, ItemEnum};
+
+        #[test]
+        fn error_on_multiple_froms() {
+            // make the enum
+            let sauce: ItemEnum = parse_quote! {
+                enum MyError {
+                    // you may not have multiple from attributes, so this should fail
+                    SomeVariant(#[from] std::io::Error, #[from] std::collection::TryReserveError)
+                }
+            };
+
+            // it should have errors
+            assert!(fields_with_from_attrs(sauce.span(), &sauce.variants).is_err());
+        }
+
+        #[test]
+        fn works_with_many_variants() {
+            // make the enum
+            let sauce: ItemEnum = parse_quote! {
+                // these variants all have one from - it should work great!
+                enum MyError {
+                    VariantOne(#[from] std::io::Error),
+                    VariantTwo(#[from] std::collection::TryReserveError),
+                    VariantThree {
+                        #[from]
+                        some_field: std::array::TryFromSliceError,
+                    }
+                }
+            };
+
+            // check it
+            assert!(fields_with_from_attrs(sauce.span(), &sauce.variants).is_ok());
+        }
+
+        #[test]
+        fn errs_with_multiple_fields() {
+            // make the enum
+            let sauce: ItemEnum = parse_quote! {
+                enum MyError {
+                    // if a variant's field has a `#[from]` attr, it MUST be the only field
+                    StructLikeVariant {
+                        #[from]
+                        some_error_type: std::io::Error,
+                        favorite_number: u32,
+                    }
+                }
+            };
+
+            assert!(fields_with_from_attrs(sauce.span(), &sauce.variants).is_err());
+        }
+    }
+
+    mod from_trait_gen_tests {
+        #[test]
+        fn two_variants_one_error() {
+            // two variants with the same From<Error> should fail
+            // assert!(result.is_err());
+        }
+    }
 }
