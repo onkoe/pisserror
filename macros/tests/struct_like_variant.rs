fafd654a188d09a5d710ee0b4a500af32ed3371d
Barrett <onkoe@protonmail.com>
2024-07-08 00:35:01 -0500

    test(macros): add test for struct-like variants
    this currently fails (because it doesn't compile and has 09235 errors)

---
+++ macros/tests/struct_like_variant.rs
@@ -0,0 +1,10 @@
+use macros::Error;
+use std::error::Error;
+
+// An error type whose one variant is struct-like
+#[derive(Debug, Error)]
+#[allow(unused)]
+enum MyError {
+    #[error("hey what's up")]
+    InformativeError { expected: i32, got: i32 },
+}
