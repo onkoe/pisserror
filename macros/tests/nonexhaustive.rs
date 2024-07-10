bf2ff3d37f09946d65740fea7cd4c31b8a070503
Barrett <onkoe@protonmail.com>
2024-07-09 23:46:36 -0500

    test: make sure nonexhaustive enums work
    fixes #4
    
    (it was always working but shhh)

---
+++ macros/tests/nonexhaustive.rs
@@ -0,0 +1,11 @@
+use macros::Error;
+use std::error::Error;
+
+/// An error type that has no variants (completely valid)
+#[derive(Debug, Error)]
+#[allow(unused)]
+#[non_exhaustive]
+enum NonexhaustiveError {
+    #[error("hi")]
+    Variant,
+}
