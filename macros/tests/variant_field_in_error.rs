3d2ddf13dcd2fa8f711f95f3098238854421ebed
Barrett <onkoe@protonmail.com>
2024-07-09 23:41:24 -0500

    test(variant): using field in error

---
+++ macros/tests/variant_field_in_error.rs
@@ -0,0 +1,25 @@
+//! This tests for error variants that use its fields within the error message.
+
+use macros::Error;
+use std::error::Error;
+
+/// An error type that has no variants (completely valid)
+#[derive(Debug, Error)]
+#[allow(unused)]
+enum FieldFmtdError {
+    #[error("my name is: {}", &name)]
+    MyStructlikeVariant { name: String },
+    #[error("my favorite color is: {_0}")] // FIXME: this is cruel
+    MyTuplelikeVariant(String),
+}
+
+#[test]
+fn construct_fmtd_err() {
+    let structlike_err = FieldFmtdError::MyStructlikeVariant {
+        name: String::from("piss"),
+    };
+    let tuplelike_err = FieldFmtdError::MyTuplelikeVariant(String::from("yellow"));
+
+    assert_eq!("my name is: piss", structlike_err.to_string());
+    assert_eq!("my favorite color is: yellow", tuplelike_err.to_string());
+}
