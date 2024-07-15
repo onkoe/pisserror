6bddd31f6d8fad3f58efb09980b6765a4fe123e5
Barrett <onkoe@protonmail.com>
2024-07-14 20:41:37 -0500

    chore(attr): remove unused types

--- macros/src/parse_enum/attr.rs
+++ macros/src/parse_enum/attr.rs
@@ -1,5 +1,4 @@
 use proc_macro2::TokenStream;
-use syn::Ident;
 
 use super::field::WrappedFieldInfo;
 
@@ -9,14 +8,4 @@ pub type FromAttribute = WrappedFieldInfo;
 pub struct ErrorAttribute {
     // TODO(#15): `Option<TokenStream>` if we allow `from` variants to go without a message?
     pub format_string: TokenStream,
-}
-
-pub enum AttributeType {
-    From(FromAttribute),
-    Error(ErrorAttribute),
-}
-
-pub struct WrappedAttr {
-    ident: Ident,
-    tokens: TokenStream,
 }
