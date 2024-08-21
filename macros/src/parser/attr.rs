94699b0e35d4670131786c781b6a5ff9b600143e
Barrett <onkoe@protonmail.com>
2024-08-21 14:25:52 -0500

    feat(parser/attr): support `#[error(transparent)]`

--- macros/src/parser/attr.rs
+++ macros/src/parser/attr.rs
@@ -5,8 +5,16 @@ use super::field::WrappedFieldInfo;
 // #[from] contains a name and type. but that's just a field!
 pub type FromAttribute = WrappedFieldInfo;
 
+/// An attribute that describes a specific error variant.
+///
+/// Can either look like `#[error("some message here")]` or `#[error(transparent)]`
+/// for variants with the `#[from]` attribute.
 #[derive(Debug)]
-pub struct ErrorAttribute {
-    // TODO(#15): `Option<TokenStream>` if we allow `from` variants to go without a message?
-    pub format_string: TokenStream,
+pub enum ErrorAttribute {
+    Stringy(TokenStream),
+    Transparent,
+}
+
+impl ErrorAttribute {
+    pub const TRANSPARENT_LITERAL: &'static str = "transparent";
 }
